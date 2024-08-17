a602628697fb00d73dab80209d60e61250a24ff2
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-08-17 15:43:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -81,7 +81,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org//file/25115719697ed91ef5672.jpg"
+    "START_IMG_URL", "https://graph.org/file/96ad3e2e58da9ded5d4ac.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://graph.org//file/389a372e8ae039320ca6c.png"
